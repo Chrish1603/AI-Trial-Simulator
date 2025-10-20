5c5c38331803d89d86fad0ab38a40b41b3533135
Chrish1603 <78516579+Chrish1603@users.noreply.github.com>
2025-10-20 16:00:18 +1300

    improving formatting by fixing import seperation

--- src/main/java/nz/ac/auckland/se206/controllers/TrialRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/TrialRoomController.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
--- src/main/java/nz/ac/auckland/se206/controllers/VerdictController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/VerdictController.java
@@ -1,7 +1,6 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
-
 import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
